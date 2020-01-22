ba3248e35fab0e0bd580eae9310f3c9e96cde8fb
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-01-22 23:14:40 +0200

    Section portfolio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
-	$('.navigation-toggle').on('click', function(){
-		$('.navigation-list').addClass('active');
+	var navButton = $('.navigation-toggle');
+	var navButtonClose = $('.navigation-button-close');
+	var navList = $('.navigation-list');
+	var navListLink = $('.navigation-list-item-link');
+
+	navButton.on('click', function(e){
+		e.preventDefault();
+		navList.addClass('active');
 	})
 
-	$('.navigation-button-close').on('click', function(){
-		$('.navigation-list').removeClass('active');
+	navButtonClose.on('click', function(){
+		navList.removeClass('active');
 	})
 
-	$('.navigation-list-item-link').on('click', function(){
-		$('.navigation-list').removeClass('active');
+	navListLink.on('click', function(){
+		navList.removeClass('active');
 	})
-})
+
+// ................Scroll................	
+
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		},800);
+	});
+});
