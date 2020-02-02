9107de8e873c38d865392041e69f5c02f8b42a3e
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-02-02 23:56:33 +0200

    Add validate form

--- js/main.js
+++ js/main.js
@@ -30,4 +30,30 @@ $(document).ready(function(){
 
 	$('#portfolio-wrap').mixItUp();
 
+// ...............ValidateJs..............
+
+	$('#contacts-form').validate({
+		rules:{
+			name: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			message: {
+				required: true
+			}
+		},
+		messages: {
+			name: 'Пожалуйста, введите свое имя',
+			email: {
+				required: 'Поле E-mail обязательное для заполнения',
+				email: 'Введите пожалуйста корректный e-mail'
+			},
+			message: 'Пожалуйста, введите текст сообщения'
+		},
+		errorClass: "error",
+		validClass: "valid"
+	})
 });
